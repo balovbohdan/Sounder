8d7ece63b535d90b748e40b05a86eb37eb6d6120
Bohdan <balovbohdan@gmail.com>
2018-04-11 18:55:42 +0300

    Example was updated

--- Sounder.js
+++ Sounder.js
@@ -17,7 +17,9 @@
  * @constructor
  *
  * @example
- * Sounder.init({
+ *
+ * // Make instance.
+ * var sounder = Sounder.init({
  *     sounds: [
  *         { name: "sound-1", loop: true, volume: 1.0 },
  *         { name: "sound-2"}
@@ -26,6 +28,15 @@
  *     preload: true,
  *     volume: 1.0
  * });
+ *
+ * // Play sounds.
+ * sounder.play("sound-1");
+ * sounder.play("sound-2");
+ *
+ * // Destruct instance after 5 seconds.
+ * // Playing sounds will be stopped.
+ * // Data of instance will be destructed.
+ * setTimeout(sounder.destruct.bind(sounder), 5000);
  */
 function Sounder(params) {
     params = params || {};
